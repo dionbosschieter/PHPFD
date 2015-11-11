3ee27762b57b34524ea7ff850d94303317e8482f
dion <dionbosschieter@gmail.com>
2015-11-11 13:33:40 +0100

    init tool

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PHPFD
+
 PHP Filesystem function call detector
+
+Little script I wrote to check if there are any php files
+using the PHP's filesystem functions instead of a custom Storage class.
+
+## Usage:
+`php phpfd.php PATH_TO_CODE`
---
+++ phpfd.php
@@ -0,0 +1,37 @@
+<?php 
+
+if (count($argv) != 2)
+    exit;
+
+$fileSystemFunctions = ["basename", "chgrp", "chmod", "chown", "clearstatcache", "copy", "delete", 
+    "dirname", "disk_free_space", "disk_total_space", "diskfreespace", "fclose", "feof", "fflush", 
+    "fgetc", "fgetcsv", "fgets", "fgetss", "file_exists", "file_get_contents", "file_put_contents", 
+    "file", "fileatime", "filectime", "filegroup", "fileinode", "filemtime", "fileowner", "fileperms", 
+    "filesize", "filetype", "flock", "fnmatch", "fopen", "fpassthru", "fputcsv", "fputs", "fread", 
+    "fscanf", "fseek", "fstat", "ftell", "ftruncate", "fwrite", "glob", "is_dir", "is_executable", 
+    "is_file", "is_link", "is_readable", "is_uploaded_file", "is_writable", "is_writeable", "lchgrp", 
+    "lchown", "link", "linkinfo", "lstat", "mkdir", "move_uploaded_file", "parse_ini_file", 
+    "parse_ini_string", "pathinfo", "pclose", "popen", "readfile", "readlink", "realpath_cache_get", 
+    "realpath_cache_size", "realpath", "rename", "rewind", "rmdir", "set_file_buffer", "stat", 
+    "symlink", "tempnam", "tmpfile", "touch", "umask", "unlink"];
+
+function checkFileSystemCalls($path, $fileSystemFunctions)
+{
+    $fileBody = file_get_contents($path);
+
+    foreach ($fileSystemFunctions as $fileSystemFunction) {
+        if (strpos($fileBody, "$fileSystemFunction(")) {
+            echo $path . ' : ' . "$fileSystemFunction(" . PHP_EOL;
+            break;
+        }
+    }
+}
+
+$directory = new RecursiveDirectoryIterator($argv[1]);
+
+foreach (new RecursiveIteratorIterator($directory) as $filename=>$current) {
+    if (is_file($current->getPathName()) === false)
+        continue;
+
+    checkFileSystemCalls($current->getPathName(), $fileSystemFunctions);
+}
